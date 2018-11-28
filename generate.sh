b7abcf8c85695a8de045b9579991261bff5c2e5c
Kevin Strishock <kevin@3io.com>
2018-11-27 21:58:09 -0800

    Adding new generate.sh commands

--- generate.sh
+++ generate.sh
@@ -1,5 +1,14 @@
-QSO > input.1
-./mkmorse.py  -t 500 -s 8 -c 20 --wav -o 1.wav < input.1 
-lame -V 9  --quiet 1.wav 1.mp3
+# Currently having issues with chirping audio -- lowering rate from 20 to 18 to try to calm that
 
-cat input.1
+
+
+./mkmorse.py  -t 600 -s 18 -c 18 --wav -o pre.wav < preamble 
+QSO > input.1
+
+./mkmorse.py  -t 600 -s 8 -c 18 --wav -o 1.wav < input.1 
+
+cat pre.wav 1.wav > combo.wav
+
+lame -V9 combo.wav combo.mp3 --tc "`cat preamble input.1`"
+
+cat preamble input.1
